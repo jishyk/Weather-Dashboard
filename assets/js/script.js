626aa7e10c975c3ad9f80ead605f8f41390fee10
Joshua King <joshua-coding@Joshuas-MacBook-Pro.local>
2023-11-17 17:57:55 -0700

    added more to script.js to fetch url

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,58 @@ function updateWeatherDisplay(data) {
 function fetchWeatherData(cityName) {
     const apiKey = '280b7b1765f40f664461ec695705a874';
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            updateWeatherDisplay(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert('Failed to retrieve weather data. Please try again.');
+        });
 }
 
+function getInfo() {
+    const newName = document.getElementById("cityInput").value.trim();
+    const cityName = document.getElementById("cityName");
+    cityName.innerHTML = `--${newName}--`;
+
+    if (newName) {
+        fetchWeatherData(newName);
+    }
+}
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             updateWeatherDisplay(data);
+//         })
+//         .catch(error => {
+//             console.error('Error fetching weather data:', error);
+//             alert('Failed to retrieve weather data. Please try again.');
+//         });
+// }
+
+// function getInfo() {
+//     const newName = document.getElementById("cityInput").value.trim();
+//     const cityName = document.getElementById("cityName");
+//     cityName.innerHTML = `--${newName}--`;
+
+//     if (newName) {
+//         fetchWeatherData(newName);
+//     }
+// }
+
+// const d = new Date();
+// const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// function checkDay(day) {
+//     return (day + d.getDay()) % 7;
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     document.getElementById(`day${i + 1}`).innerHTML = weekDay[checkDay(i)];
+// }
